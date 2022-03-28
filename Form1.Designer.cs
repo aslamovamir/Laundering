2c2c4ba31b898224ad61caad3626e5378cf01365
Amir Aslamov <amiraslamov@usf.edu>
2022-03-28 18:43:22 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Laundering
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Convert_BTN = new System.Windows.Forms.Button();
+            this.Input_TXTBX = new System.Windows.Forms.TextBox();
+            this.YenDollars_TXTBX = new System.Windows.Forms.TextBox();
+            this.PoundYen_TXTBX = new System.Windows.Forms.TextBox();
+            this.EuroPound_TXTBX = new System.Windows.Forms.TextBox();
+            this.YuanEuro_TXTBX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DollarsYen_TXTBX = new System.Windows.Forms.TextBox();
+            this.YenPound_TXTBX = new System.Windows.Forms.TextBox();
+            this.PoundEuro_TXTBX = new System.Windows.Forms.TextBox();
+            this.EuroYuan_TXTBX = new System.Windows.Forms.TextBox();
+            this.YuanDollars_TXTBX = new System.Windows.Forms.TextBox();
+            this.DollarsYuan_TXTBX = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laundering Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(566, 338);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noto Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter US dollars amount:";
+            // 
+            // Convert_BTN
+            // 
+            this.Convert_BTN.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convert_BTN.Location = new System.Drawing.Point(382, 378);
+            this.Convert_BTN.Name = "Convert_BTN";
+            this.Convert_BTN.Size = new System.Drawing.Size(91, 29);
+            this.Convert_BTN.TabIndex = 3;
+            this.Convert_BTN.Text = "Convert";
+            this.Convert_BTN.UseVisualStyleBackColor = true;
+            this.Convert_BTN.Click += new System.EventHandler(this.Convert_BTN_Click);
+            // 
+            // Input_TXTBX
+            // 
+            this.Input_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input_TXTBX.Location = new System.Drawing.Point(497, 104);
+            this.Input_TXTBX.Name = "Input_TXTBX";
+            this.Input_TXTBX.Size = new System.Drawing.Size(164, 25);
+            this.Input_TXTBX.TabIndex = 4;
+            // 
+            // YenDollars_TXTBX
+            // 
+            this.YenDollars_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YenDollars_TXTBX.Location = new System.Drawing.Point(539, 148);
+            this.YenDollars_TXTBX.Name = "YenDollars_TXTBX";
+            this.YenDollars_TXTBX.Size = new System.Drawing.Size(122, 25);
+            this.YenDollars_TXTBX.TabIndex = 5;
+            // 
+            // PoundYen_TXTBX
+            // 
+            this.PoundYen_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PoundYen_TXTBX.Location = new System.Drawing.Point(539, 193);
+            this.PoundYen_TXTBX.Name = "PoundYen_TXTBX";
+            this.PoundYen_TXTBX.Size = new System.Drawing.Size(122, 25);
+            this.PoundYen_TXTBX.TabIndex = 6;
+            // 
+            // EuroPound_TXTBX
+            // 
+            this.EuroPound_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EuroPound_TXTBX.Location = new System.Drawing.Point(539, 237);
+            this.EuroPound_TXTBX.Name = "EuroPound_TXTBX";
+            this.EuroPound_TXTBX.Size = new System.Drawing.Size(122, 25);
+            this.EuroPound_TXTBX.TabIndex = 7;
+            // 
+            // YuanEuro_TXTBX
+            // 
+            this.YuanEuro_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YuanEuro_TXTBX.Location = new System.Drawing.Point(539, 286);
+            this.YuanEuro_TXTBX.Name = "YuanEuro_TXTBX";
+            this.YuanEuro_TXTBX.Size = new System.Drawing.Size(122, 25);
+            this.YuanEuro_TXTBX.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Noto Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(360, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "US dollars to Yen";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Noto Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(360, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Yen to Pound";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Noto Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(360, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 26);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Pound to Euro";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Noto Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(360, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 26);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Euro to Yuan";
+            // 
+            // DollarsYen_TXTBX
+            // 
+            this.DollarsYen_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DollarsYen_TXTBX.Location = new System.Drawing.Point(207, 150);
+            this.DollarsYen_TXTBX.Name = "DollarsYen_TXTBX";
+            this.DollarsYen_TXTBX.Size = new System.Drawing.Size(117, 25);
+            this.DollarsYen_TXTBX.TabIndex = 13;
+            // 
+            // YenPound_TXTBX
+            // 
+            this.YenPound_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YenPound_TXTBX.Location = new System.Drawing.Point(207, 193);
+            this.YenPound_TXTBX.Name = "YenPound_TXTBX";
+            this.YenPound_TXTBX.Size = new System.Drawing.Size(117, 25);
+            this.YenPound_TXTBX.TabIndex = 14;
+            // 
+            // PoundEuro_TXTBX
+            // 
+            this.PoundEuro_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PoundEuro_TXTBX.Location = new System.Drawing.Point(207, 239);
+            this.PoundEuro_TXTBX.Name = "PoundEuro_TXTBX";
+            this.PoundEuro_TXTBX.Size = new System.Drawing.Size(117, 25);
+            this.PoundEuro_TXTBX.TabIndex = 15;
+            // 
+            // EuroYuan_TXTBX
+            // 
+            this.EuroYuan_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EuroYuan_TXTBX.Location = new System.Drawing.Point(207, 286);
+            this.EuroYuan_TXTBX.Name = "EuroYuan_TXTBX";
+            this.EuroYuan_TXTBX.Size = new System.Drawing.Size(117, 25);
+            this.EuroYuan_TXTBX.TabIndex = 16;
+            // 
+            // YuanDollars_TXTBX
+            // 
+            this.YuanDollars_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YuanDollars_TXTBX.Location = new System.Drawing.Point(207, 331);
+            this.YuanDollars_TXTBX.Name = "YuanDollars_TXTBX";
+            this.YuanDollars_TXTBX.Size = new System.Drawing.Size(117, 25);
+            this.YuanDollars_TXTBX.TabIndex = 17;
+            // 
+            // DollarsYuan_TXTBX
+            // 
+            this.DollarsYuan_TXTBX.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DollarsYuan_TXTBX.Location = new System.Drawing.Point(539, 331);
+            this.DollarsYuan_TXTBX.Name = "DollarsYuan_TXTBX";
+            this.DollarsYuan_TXTBX.Size = new System.Drawing.Size(122, 25);
+            this.DollarsYuan_TXTBX.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Noto Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(348, 330);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 26);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Yuan to US dollars";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.DollarsYuan_TXTBX);
+            this.Controls.Add(this.YuanDollars_TXTBX);
+            this.Controls.Add(this.EuroYuan_TXTBX);
+            this.Controls.Add(this.PoundEuro_TXTBX);
+            this.Controls.Add(this.YenPound_TXTBX);
+            this.Controls.Add(this.DollarsYen_TXTBX);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.YuanEuro_TXTBX);
+            this.Controls.Add(this.EuroPound_TXTBX);
+            this.Controls.Add(this.PoundYen_TXTBX);
+            this.Controls.Add(this.YenDollars_TXTBX);
+            this.Controls.Add(this.Input_TXTBX);
+            this.Controls.Add(this.Convert_BTN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Convert_BTN;
+        private System.Windows.Forms.TextBox Input_TXTBX;
+        private System.Windows.Forms.TextBox YenDollars_TXTBX;
+        private System.Windows.Forms.TextBox PoundYen_TXTBX;
+        private System.Windows.Forms.TextBox EuroPound_TXTBX;
+        private System.Windows.Forms.TextBox YuanEuro_TXTBX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox DollarsYen_TXTBX;
+        private System.Windows.Forms.TextBox YenPound_TXTBX;
+        private System.Windows.Forms.TextBox PoundEuro_TXTBX;
+        private System.Windows.Forms.TextBox EuroYuan_TXTBX;
+        private System.Windows.Forms.TextBox YuanDollars_TXTBX;
+        private System.Windows.Forms.TextBox DollarsYuan_TXTBX;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laundering
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private decimal DollarsToYen(decimal dollars)
+        {
+            return dollars * (decimal)108.22;
+        }
+
+        private decimal YenToPound(decimal yen)
+        {
+            return yen * (decimal)0.0071;
+        }
+
+        private decimal PoundToEuro(decimal pound)
+        {
+            return pound * (decimal)1.16;
+        }
+
+        private decimal EuroToYuan(decimal euro)
+        {
+            return euro * (decimal)7.85;
+        }
+
+        private decimal YuanToDollars(decimal yuan)
+        {
+            return yuan * (decimal)0.14;
+        }
+
+        private void Convert_BTN_Click(object sender, EventArgs e)
+        {
+            // let's take the input from the textbox
+            if (Input_TXTBX.Text != "")
+            {
+                decimal input = 0;
+                try
+                {
+                    input = decimal.Parse(Input_TXTBX.Text);
+
+                    //we will directly change the textboxes for the different currencies 
+                    //first convert dollars to Yen
+                    DollarsYen_TXTBX.Text = input.ToString();
+                    YenDollars_TXTBX.Text = DollarsToYen(input).ToString();
+
+                    //now convert Yen to Pound
+                    YenPound_TXTBX.Text = YenDollars_TXTBX.Text;
+                    PoundYen_TXTBX.Text = YenToPound(Math.Round(decimal.Parse(YenPound_TXTBX.Text), 1)).ToString();
+
+                    //now convert Pound to Euro
+                    PoundEuro_TXTBX.Text = PoundYen_TXTBX.Text;
+                    EuroPound_TXTBX.Text = PoundToEuro(Math.Round(decimal.Parse(PoundEuro_TXTBX.Text), 1)).ToString();
+
+                    //now convert Euro to Yuan
+                    EuroYuan_TXTBX.Text = EuroPound_TXTBX.Text;
+                    YuanEuro_TXTBX.Text = EuroToYuan(Math.Round(decimal.Parse(EuroYuan_TXTBX.Text), 1)).ToString();
+
+                    //now convert Yuan to Dollars
+                    YuanDollars_TXTBX.Text = YuanEuro_TXTBX.Text;
+                    DollarsYuan_TXTBX.Text = YuanToDollars(Math.Round(decimal.Parse(YuanDollars_TXTBX.Text), 1)).ToString();
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid Input!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input!");
+            }
+        }
+    }
+}
